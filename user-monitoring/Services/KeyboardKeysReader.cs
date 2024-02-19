d1d6e6ba03e0539d5ebe901b896409807fa3d6d5
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2024-02-19 20:31:37 +0300

    Added KeyboardKeysReader interface. Closed #5 (#6)

--- user-monitoring/Services/KeyboardKeysReader.cs
+++ user-monitoring/Services/KeyboardKeysReader.cs
@@ -1,7 +1,12 @@
-﻿namespace user_monitoring.Services
-{
-    public class KeyboardKeysReader
-    {
+﻿using user_monitoring.Services.Interfaces;
 
+namespace user_monitoring.Services
+{
+    public class KeyboardKeysReader : IKeyboardKeysReader
+    {
+        public uint GetKeyCodePressedKey()
+        {
+            return 0;
+        }
     }
 }
