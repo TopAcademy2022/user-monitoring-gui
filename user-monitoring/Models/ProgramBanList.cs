71723b0b80ffa60e1d4fa86ac0dfdef2c7183660
TopAcademy2022 <popov_r_top@outlook.com>
2024-02-19 21:06:19 +0300

    Added ProgramBanListService class. Closed #10

--- user-monitoring/Models/ProgramBanList.cs
+++ user-monitoring/Models/ProgramBanList.cs
@@ -2,6 +2,26 @@
 {
     public class ProgramBanList
     {
+        private List<string> _programBanList;
 
+        public ProgramBanList()
+        {
+            this._programBanList = new List<string>();
+        }
+
+        public List<string> GetProgramBanList()
+        {
+            return this._programBanList;
+        }
+
+        public void AddProgram(string programName)
+        {
+
+        }
+
+        public void RemoveProgram(string programName)
+        {
+
+        }
     }
 }
