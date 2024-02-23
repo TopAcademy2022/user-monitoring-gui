0f551c9ee2dcc7006823ae7d1c79282a32814136
LONDON3149\strel <strellook@ya.ru>
2024-02-23 16:03:52 +0300

    Added method GetKeyCodePressedKey

--- user-monitoring/Services/KeyboardKeysReader.cs
+++ user-monitoring/Services/KeyboardKeysReader.cs
@@ -1,12 +1,33 @@
 ﻿using user_monitoring.Services.Interfaces;
 
+using Hooks;
+using System.Drawing;
+
 namespace user_monitoring.Services
 {
     public class KeyboardKeysReader : IKeyboardKeysReader
     {
-        public uint GetKeyCodePressedKey()
+        public uint Key {  get; set; }
+        public uint GetKeyCodePressedKey ()
         {
-            return 0;
+            Hooks.KBDHook.KeyDown += new Hooks.KBDHook.HookKeyPress( KBDHook_KeyDown );
+            Hooks.KBDHook.KeyUp += new Hooks.KBDHook.HookKeyPress( KBDHook_KeyUp );
+            Hooks.KBDHook.LocalHook = false;
+            Hooks.KBDHook.InstallHook();
+
+            return Key;
+        }
+
+        void KBDHook_KeyUp ( Hooks.LLKHEventArgs e )
+        {
+            this.Key = e.LkCode;
+        }
+
+        string text = "";
+
+        void KBDHook_KeyDown ( Hooks.LLKHEventArgs e )
+        {
+            this.Key = e.LkCode;
         }
     }
 }
