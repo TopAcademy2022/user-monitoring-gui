8c3b3eb2d9a226ddaccf675b79d53c9c9aad56d6
SugarSalt9 <121046903+SugarSalt9@users.noreply.github.com>
2024-03-01 18:44:45 +0300

    Implemented the PrintMenu method. Closed #3 (#24)

--- user-monitoring/Services/ConsoleUserInterface.cs
+++ user-monitoring/Services/ConsoleUserInterface.cs
@@ -6,7 +6,58 @@ namespace user_monitoring.Services
     {
         public void PrintMenu()
         {
+            const uint NUMBER_EXIT_MENU_ELEMENT = 6;
+            int numberMenuElement = 0;
 
+            while (numberMenuElement != NUMBER_EXIT_MENU_ELEMENT)
+            {
+                Console.WriteLine("***********************************************");
+                Console.WriteLine("***********************************************");
+                Console.WriteLine("\t====== Меню ======\n");
+                Console.WriteLine("1. Настроить");
+                Console.WriteLine("2. Редактировать список запрещённых слов");
+                Console.WriteLine("3. Редактировать список запрещённых программ");
+                Console.WriteLine("4. Запустить");
+                Console.WriteLine("5. Показать статистику");
+                Console.WriteLine("6. Выход\n");
+                Console.WriteLine("***********************************************");
+
+                Console.Write("Выберите пункт меню: ");
+                ConsoleKeyInfo userInputSymbol;
+
+                do
+                {
+                    userInputSymbol = Console.ReadKey(true);
+                }
+                while (userInputSymbol.Key < ConsoleKey.D0 || userInputSymbol.Key > ConsoleKey.D6);
+
+                numberMenuElement = Convert.ToInt32(userInputSymbol.KeyChar.ToString());
+
+                Console.Clear();
+
+                switch (numberMenuElement)
+                {
+                case 1:
+                    PrintMenuSettings();
+                    break;
+
+                case 2:
+                    PrintMenuWordBanList();
+                    break;
+
+                case 3:
+                    PrintMenuProgramBanList();
+                    break;
+
+                case 4:
+                    RunProgram();
+                    break;
+
+                case 5:
+                    PrintMenuStatistic();
+                    break;
+                }
+            }
         }
 
         public void PrintMenuSettings()
