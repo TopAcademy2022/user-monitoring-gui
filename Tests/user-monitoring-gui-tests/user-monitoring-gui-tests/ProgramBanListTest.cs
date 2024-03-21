35997ddfca9c88662781a103f54922801700d3ac
LONDON3149\strel <strellook@ya.ru>
2024-03-21 20:48:08 +0300

    Added tests for ProgramBanList. Closed #36

---
+++ Tests/user-monitoring-gui-tests/user-monitoring-gui-tests/ProgramBanListTest.cs
@@ -0,0 +1,54 @@
+﻿using user_monitoring_gui.Models;
+
+namespace user_monitoring_gui_tests
+{
+    /*!
+     *  \test - Class ProgramBanListTest for testing methods 
+     */
+    public class ProgramBanListTest
+    {
+        private ProgramBanList programBanList = new ProgramBanList(); /// - Implementation of the class under test
+
+        /*!
+         *  \test - Test Method for checking the transferred sheet for initialization 
+         */
+        [Fact]
+        public void GetProgramBanListTest( )
+        {
+            var result = programBanList.GetProgramBanList();
+            Assert.NotNull(result);
+        }
+
+        /*!
+         *  \test - Test Verification method adds value to the list
+         */
+        [Fact]
+        public void AddProgramTest()
+        {
+            programBanList.AddProgram("Test1");
+            programBanList.AddProgram("Test2");
+            programBanList.AddProgram("Test3");
+
+            var result = programBanList.GetProgramBanList();
+
+            Assert.Contains( "Test2", result );
+        }
+
+        /*!
+         *  \test - Test Method for checking whether a value is removed from a list
+         */
+        [Fact]
+        public void RemoveProgramTest()
+        {
+            programBanList.AddProgram("Test1");
+            programBanList.AddProgram("Test2");
+            programBanList.AddProgram("Test3");
+
+            programBanList.RemoveProgram("Test2");
+
+            var result = programBanList.GetProgramBanList();
+
+            Assert.DoesNotContain("Test2", result);
+        }
+    }
+}
