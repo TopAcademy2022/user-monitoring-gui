8b03893b04155a1da08291f6f42c18a88d11ea3e
LONDON3149\strel <strellook@ya.ru>
2024-03-22 21:55:38 +0300

    Added tests for RunProgramReader. Closed #41

---
+++ Tests/user-monitoring-gui-tests/user-monitoring-gui-tests/RunProgramReaderTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using user_monitoring_gui.Services;
+
+namespace user_monitoring_gui_tests
+{
+    /*!
+     *  \test - Class RunProgramReaderTest for testing methods 
+     */
+    public class RunProgramReaderTest
+    {
+        private RunProgramReader runProgramReader = new RunProgramReader(); /// - Implementation of the class under test
+
+        /*!
+         *  \test - test method to check if a program is open
+         */
+        [Fact]
+        public void CheckRunProgramTest() 
+        {
+            Process p = Process.Start( "notepad" );
+            var rezult = runProgramReader.CheckRunProgram( "notepad" );
+            Assert.True( rezult );
+            runProgramReader.KillProgram( "notepad" );
+        }
+
+        /*!
+         *  \test - Test method to check if it closes open program
+         */
+        [Fact]
+        public void KillProgramTest()
+        {
+            Process p = Process.Start( "notepad" );
+            var rezult = runProgramReader.KillProgram( "notepad" );
+            Assert.True( rezult );
+        }
+    }
+}
