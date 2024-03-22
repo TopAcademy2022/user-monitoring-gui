d7970ee5c60672692c41162714f550304c6426da
HecuseS <112653338+HecuseS@users.noreply.github.com>
2024-03-22 20:34:37 +0300

    Implemented the SaveWordBanList method. Closed #34

--- user-monitoring-gui/Program.cs
+++ user-monitoring-gui/Program.cs
@@ -1,13 +1,1 @@
-﻿using user_monitoring_gui.Models;
-using user_monitoring_gui.Services.Interfaces;
-
-Console.WriteLine("Hello, World!");
-
-WordBanList wordBanList = new WordBanList();
-wordBanList.AddWord("Tom");
-wordBanList.AddWord("Tom");
-wordBanList.AddWord("Tom");
-
-
-WordBanListService wordBanService = new WordBanListService();
-wordBanService.Save(wordBanList, true);
+﻿Console.WriteLine("Hello, World!");
