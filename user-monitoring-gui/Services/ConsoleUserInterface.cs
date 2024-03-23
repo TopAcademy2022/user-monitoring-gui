6d3d3980c1bdfa6262ee00e0f4813339ee9ccf3c
Mi-stik31 <25-medvedev@mail.ru>
2024-03-23 19:55:02 +0300

    Implemented the PrintMenuSettings method. Closed #4

--- user-monitoring-gui/Services/ConsoleUserInterface.cs
+++ user-monitoring-gui/Services/ConsoleUserInterface.cs
@@ -1,11 +1,20 @@
-﻿using user_monitoring_gui.Services.Interfaces;
+﻿using user_monitoring_gui.Models;
+using user_monitoring_gui.Services.Interfaces;
 
-namespace user_monitoring_gui.Services
+namespace user_monitoring.Services
 {
     public class ConsoleUserInterface : IUserInterface
     {
+        private Setting _setting;
+
+        public ConsoleUserInterface(Setting setting)
+        {
+            this._setting = setting;
+        }
+
         public void PrintMenu()
         {
+
             const uint NUMBER_EXIT_MENU_ELEMENT = 6;
             int numberMenuElement = 0;
 
@@ -37,32 +46,86 @@ namespace user_monitoring_gui.Services
 
                 switch (numberMenuElement)
                 {
-                case 1:
-                    PrintMenuSettings();
-                    break;
+                    case 1:
+                        PrintMenuSettings();
+                        break;
 
-                case 2:
-                    PrintMenuWordBanList();
-                    break;
+                    case 2:
+                        PrintMenuWordBanList();
+                        break;
 
-                case 3:
-                    PrintMenuProgramBanList();
-                    break;
+                    case 3:
+                        PrintMenuProgramBanList();
+                        break;
 
-                case 4:
-                    RunProgram();
-                    break;
+                    case 4:
+                        RunProgram();
+                        break;
 
-                case 5:
-                    PrintMenuStatistic();
-                    break;
+                    case 5:
+                        PrintMenuStatistic();
+                        break;
                 }
             }
         }
 
         public void PrintMenuSettings()
         {
+            int numberMenuElement = 0;
+            const uint NUMBER_EXIT_MENU_ELEMENT = 5;
+            ConsoleKeyInfo userInputSymbol;
+            while (numberMenuElement != NUMBER_EXIT_MENU_ELEMENT)
+            {
+                Console.Clear();
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
 
+                const char SYMBOL_СHECMARK = '\u2705';
+                const char SYMBOL_СROSS = '\u274E';
+                char OnСonditions;
+                Console.WriteLine("\t\t\tНастройки:\n");
+
+                Console.Write("1. Считывать нажатия клавиш. ");
+                OnСonditions = (this._setting.ReadKeyboardInput == true) ? SYMBOL_СHECMARK : SYMBOL_СROSS;
+                Console.WriteLine(OnСonditions);
+
+                Console.Write("2. Считывать список запущенных приложений. ");
+                OnСonditions = (this._setting.ReadRunPrograms == true) ? SYMBOL_СHECMARK : SYMBOL_СROSS;
+                Console.WriteLine(OnСonditions);
+
+                Console.Write("3. Анализ запрещённых слов. ");
+                OnСonditions = (this._setting.AnalysisProhibitedWords == true) ? SYMBOL_СHECMARK : SYMBOL_СROSS;
+                Console.WriteLine(OnСonditions);
+
+                Console.Write("4. Анализ запрещённых программ. ");
+                OnСonditions = (this._setting.AnalysisProhibitedPrograms == true) ? SYMBOL_СHECMARK : SYMBOL_СROSS;
+                Console.WriteLine(OnСonditions);
+                Console.WriteLine("5.Назад. ");
+
+                do
+                {
+                    userInputSymbol = Console.ReadKey(true);
+                }
+                while (userInputSymbol.Key < ConsoleKey.D0 || userInputSymbol.Key > ConsoleKey.D5);
+
+                numberMenuElement = Convert.ToInt32(userInputSymbol.KeyChar.ToString());
+
+                switch (numberMenuElement)
+                {
+                    case 1:
+                        this._setting.ReadKeyboardInput ^= true;
+                        break;
+                    case 2:
+                        this._setting.ReadRunPrograms ^= true;
+                        break;
+                    case 3:
+                        this._setting.AnalysisProhibitedWords ^= true;
+                        break;
+                    case 4:
+                        this._setting.AnalysisProhibitedPrograms ^= true;
+                        break;
+                }
+
+            }
         }
 
         public void PrintMenuWordBanList()
