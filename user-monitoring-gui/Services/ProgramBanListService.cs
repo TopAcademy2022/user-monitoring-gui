85305394409510b4695646d8c43d57fd2b7c0ed3
Andreyts31 <novitskiy19882907@mail.ru>
2024-03-27 19:14:22 +0300

    Implemented the SaveProgramBanList method. Closed #17

--- user-monitoring-gui/Services/ProgramBanListService.cs
+++ user-monitoring-gui/Services/ProgramBanListService.cs
@@ -1,12 +1,44 @@
-﻿using user_monitoring_gui.Models;
+﻿using System.Text;
+using user_monitoring_gui.Models;
 using user_monitoring_gui.Services.Interfaces;
 
 namespace user_monitoring_gui.Services
 {
-    public class ProgramBanListService : IProgramBanListService
+    public class ProgramBanListService:IProgramBanListService
     {
-        public bool Save(ProgramBanList programBanList)
+
+   private IServerRequest _serverReqwest;
+
+        public ProgramBanListService(IServerRequest serverReqwest)
         {
+            this._serverReqwest = serverReqwest;
+        }
+        public bool Save(ProgramBanList programBanList, DataStorageArea dataStorageArea)
+        {
+            /* ! decoding incoming data into a byte array */
+
+            var bytes = programBanList.GetProgramBanList().Select(i => Encoding.Default.GetBytes($"{i}\n")).ToArray();
+
+            switch (dataStorageArea)
+            {
+                case DataStorageArea.FILE:
+                    using (FileStream fstream = new FileStream("ProgramBanList.txt", FileMode.Append))
+                    {
+                        foreach (var item in bytes)
+                        {
+                            fstream.Write(item, 0, item.Length);
+                        }
+                    }
+                    return true;
+
+                case DataStorageArea.SERVER:
+                    foreach (var item in bytes)
+                    {
+                        this._serverReqwest.SendData(item);
+                    }
+                    return true;
+            }
+
             return false;
         }
 
