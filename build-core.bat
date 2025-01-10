363990db1c1e415e6c8355bef874e2403652c555
TopAcademy2022 <popov_r_top@outlook.com>
2025-01-10 20:31:42 +0300

    Updated build core script. Closed #71

--- build-core.bat
+++ build-core.bat
@@ -1,33 +1,53 @@
-:: Remarks TODO: Update build type, create paths and update relative paths
 :: Remarks Print all information on console
 @echo ON
 
+:: Remarks Save start directory
+set "START_DIR=%cd%"
+:: Remarks Set default export directory strategy (Create or ignore)
+set "EXPORT_DIR_STRATEGY=Create"
+:: Remarks Set default exit strategy (AfterLastCommand or none)
+set "EXIT_STRATEGY=AfterLastCommand"
+:: Remarks Set build type (Release or Debug)
+set "BUILD_TYPE=Release"
+:: Remarks Set build type (Release or Debug)
+set "DOTNET_VERSION=net6.0"
+
 :: Remarks Folder name with core library
-set CORE_DIR=./core/user-monitoring-core/
-set CORE_LIB_NAME=user-monitoring-core.dll
+set "CORE_BUILD_DIR=./core/user-monitoring-core/build"
+:: Remarks Library name
+set "CORE_LIB_NAME=user-monitoring-core.dll"
+:: Remarks Core library build script name
+set "CORE_BUILD_SCRIPT_NAME=build-windows.bat"
+:: Remarks Library export directory
+set "LIB_EXPORT_DIR=./user-monitoring-gui/bin/%BUILD_TYPE%/%DOTNET_VERSION%/"
 
-if exist '%CORE_DIR%' (
-	echo Core directory exist
+if "%EXPORT_DIR_STRATEGY%"=="Create" (
+    REM Create path for export core library
+    mkdir "%LIB_EXPORT_DIR%" /p
 )
-else (
-	:: Remarks Call init script
-	cd %CORE_DIR%
-	mkdir build
-	cd build
-	
-	:: Remarks Run cmake build for core library
-	set CMAKE_PRE_BUILD_COMMAND="cmake .."
-	set CMAKE_BUILD_COMMAND="cmake --build ."
-	powershell -Command %CMAKE_PRE_BUILD_COMMAND%
-	powershell -Command %CMAKE_BUILD_COMMAND%
 
-	cd Debug
-	if exist '%CORE_LIB_NAME%' (
-		echo Core library exist
-	)
-	else (
-		copy %CORE_LIB_NAME% ..\..\..\..\
-		cd ../../../../
-		move %CORE_LIB_NAME% ./user-monitoring-gui/bin/Debug/net6.0/
-	)
+if exist "%LIB_EXPORT_DIR%" (
+    if exist "%CORE_BUILD_DIR%" (
+        cd "%CORE_BUILD_DIR%"
+        :: Remarks Call build script (default Release version)
+        call "%CORE_BUILD_SCRIPT_NAME%" "none"
+        
+        if exist "%CORE_LIB_NAME%" (
+            :: Remarks Call build script (default Release version)
+            copy "%CORE_LIB_NAME%" "%START_DIR%"
+            
+            cd "%START_DIR%"
+            move "%CORE_LIB_NAME%" "%LIB_EXPORT_DIR%"
+            
+            if "%EXIT_STRATEGY%"=="AfterLastCommand" (
+                exit 0
+            )
+        ) else (
+            echo Core library not exist && exit 1
+        )
+    ) else (
+        echo Core library directory not exist && exit 1
+    )
+) else (
+    echo Project build directory not exist && exit 1
 )
---
+++ build-core.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Print all information on console
+set -x
+
+# Save start directory
+START_DIR=$(pwd)
+
+# Set default export directory strategy (Create or ignore)
+EXPORT_DIR_STRATEGY="Create"
+
+# Set default exit strategy (AfterLastCommand or none)
+EXIT_STRATEGY="AfterLastCommand"
+
+# Set build type (Release or Debug)
+BUILD_TYPE="Release"
+
+# Set .NET version
+DOTNET_VERSION="net6.0"
+
+# Folder name with core library
+CORE_BUILD_DIR="./core/user-monitoring-core/build"
+
+# Library name
+CORE_LIB_NAME="user-monitoring-core.dll"
+
+# Core library build script name
+CORE_BUILD_SCRIPT_NAME="build-windows.sh"
+
+# Library export directory
+LIB_EXPORT_DIR="./user-monitoring-gui/bin/${BUILD_TYPE}/${DOTNET_VERSION}/"
+
+if [ "$EXPORT_DIR_STRATEGY" == "Create" ]; then
+    # Create path for export core library
+    mkdir -p "$LIB_EXPORT_DIR"
+fi
+
+if [ -d "$LIB_EXPORT_DIR" ]; then
+    if [ -d "$CORE_BUILD_DIR" ]; then
+        cd "$CORE_BUILD_DIR" || exit
+
+        # Call build script (default Release version)
+        ./"$CORE_BUILD_SCRIPT_NAME" "none"
+
+        if [ -f "$CORE_LIB_NAME" ]; then
+            # Copy core library to start directory
+            cp "$CORE_LIB_NAME" "$START_DIR"
+
+            cd "$START_DIR" || exit
+            mv "$CORE_LIB_NAME" "$LIB_EXPORT_DIR"
+
+            if [ "$EXIT_STRATEGY" == "AfterLastCommand" ]; then
+                exit 0
+            fi
+        else
+            echo "Core library does not exist"
+            exit 1
+        fi
+    else
+        echo "Core library directory does not exist"
+        exit 1
+    fi
+else
+    echo "Project build directory does not exist"
+    exit 1
+fi
