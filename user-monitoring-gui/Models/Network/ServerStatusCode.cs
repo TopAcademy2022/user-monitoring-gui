2896e6ed786efe285f8f73ae891a7237fc2f0c58
London3149-train <strellook@yandex.ru>
2025-02-10 21:22:21 +0300

    Added documentation for ServerStatusCode. #Closed 56

--- user-monitoring-gui/Models/Network/ServerStatusCode.cs
+++ user-monitoring-gui/Models/Network/ServerStatusCode.cs
@@ -1,9 +1,18 @@
 ﻿namespace user_monitoring_gui.Models.Network
 {
+
+    /*!
+         * @brief Enumeration representing server status codes
+         * @param programBanList - the class is passed
+         * @return enumeration is used to indicate the current status of the server.
+         */
     public enum ServerStatusCode
     {
         Good = 0,
         Bad = 1,
         Unknown = 2
     }
+
+  
+
 }
