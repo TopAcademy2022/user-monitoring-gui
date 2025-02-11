07bfd300250efd3bd8537a57665933ac0f872dd9
Artem020285 <tarasenko020285@gmail.com>
2025-02-11 20:40:18 +0300

    Added documentation ProgramBanList. Closed #56

--- user-monitoring-gui/Services/ProgramBanListService.cs
+++ user-monitoring-gui/Services/ProgramBanListService.cs
@@ -4,24 +4,40 @@ using user_monitoring_gui.Services.Interfaces;
 
 namespace user_monitoring_gui.Services
 {
+
+    /*!
+      * @brief Service responsible for managing the Program Ban List, including saving and loading data
+      * from different storage areas (file or server).
+      */
     public class ProgramBanListService : IProgramBanListService
     {
         private IServerRequest _serverRequest;
 
+
+        /*!
+         * @brief Initializes a new instance of the class.
+         * @param serverRequest An instance of used for server communication.
+         */
         public ProgramBanListService (IServerRequest serverRequest)
         {
             this._serverRequest = serverRequest;
         }
 
+        /*!
+         * @brief Saves the program ban list to the specified data storage area.
+         * @param[in] programBanList object to save.
+         * @param[in] dataStorageArea 	Selecting a storage location option.
+         * @return returns the true when the data is successfully saved.
+         */
         public bool Save(ProgramBanList programBanList, DataStorageArea dataStorageArea)
         {
-            /* ! decoding incoming data into a byte array */
 
             var bytes = programBanList.GetProgramBanList().Select(i => Encoding.Default.GetBytes($"{i}\n")).ToArray();
 
             switch (dataStorageArea)
             {
                 case DataStorageArea.FILE:
+
                     using (FileStream fstream = new FileStream("ProgramBanList.txt", FileMode.Append))
                     {
                         foreach (var item in bytes)
@@ -33,6 +49,7 @@ namespace user_monitoring_gui.Services
                     return true;
 
                 case DataStorageArea.SERVER:
+                    
                     foreach (var item in bytes)
                     {
                         this._serverRequest.SendData(item);
@@ -44,39 +61,35 @@ namespace user_monitoring_gui.Services
             return false;
         }
 
+
         /*!
-         * @brief Load method reads data for ProgramBanList
-         * @param programBanList - the class is passed
-         * @param dataStorageArea - the class is passed
-         * @return True if data was received, false otherwise.
-         */
+        * @brief Loads the program ban list from the specified data storage area.
+        * @param[in] programBanList object to save.
+        * @param[in] dataStorageArea Selecting a storage location option.
+        * @return returns the true when the data is successfully loaded.
+        */
         public bool Load (ProgramBanList programBanList, DataStorageArea dataStorageArea)
         {
             switch(dataStorageArea)
             {
             case DataStorageArea.FILE:
 
-                List<string> lines = File.ReadAllLines("ProgramBanList.txt").ToList();
+                    List<string> lines = File.ReadAllLines("ProgramBanList.txt").ToList();
 
-                foreach(var line in lines)
+                    foreach (var line in lines)
                 {
                     programBanList.AddProgram(line);
                 }
 
                 return true;
 
-            case DataStorageArea.SERVER:
+            case DataStorageArea.SERVER: 
 
-                var rezult = this._serverRequest.LoadData(programBanList.GetType()).GetData();
+                    var rezult = this._serverRequest.LoadData(programBanList.GetType()).GetData();
 
-                programBanList.AddProgram(Encoding.Default.GetString(rezult));
-
-                //TODO: Update tu this
-                //foreach( var line in byat )
-                //{
-                //    programBanList.AddProgram( Encoding.Default.GetString(line));
-                //}
-                return true;
+                    programBanList.AddProgram(Encoding.Default.GetString(rezult));
+            
+                    return true;
             }
 
             return false;
