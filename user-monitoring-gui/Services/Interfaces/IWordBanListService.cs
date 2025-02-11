d02e6e009ee7054001147972f177314039f64812
Artem020285 <98900637+Artem020285@users.noreply.github.com>
2025-02-11 20:46:34 +0300

    Added documentation ProgramBanList. Closed #56 (#76)

--- user-monitoring-gui/Services/Interfaces/IWordBanListService.cs
+++ user-monitoring-gui/Services/Interfaces/IWordBanListService.cs
@@ -4,8 +4,18 @@ namespace user_monitoring_gui.Services.Interfaces
 {
     public interface IWordBanListService
     {
+        /*!
+       * @brief this method shows if there are saves or not.
+       * @param[in] wordBanList defines a word list.
+       * @param[in] dataStorageArea defines the data storage area.
+       */       
         public bool Save(WordBanList wordBanList, DataStorageArea dataStorageArea);
 
+        /*!
+       * @brief a method that has a comparison of whether there is a load or not.
+       * @param[in] wordBanList defines a word list.
+       * @param[in] dataStorageArea defines the data storage area.
+       */
         public bool Load(WordBanList wordBanList, DataStorageArea dataStorageArea);
     }
 }
